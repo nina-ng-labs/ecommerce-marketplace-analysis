f75ac030b68224561c1b28d5cff069d55ae33d33
Nina Ng <terrynguyen19881215@gmail.com>
2026-07-30 21:54:55 +0700

    Add table creation script

---
+++ sql/00_create_tables.sql
@@ -0,0 +1,7 @@
+-- ============================================
+-- Project: E-commerce Marketplace Analysis
+-- File: 00_create_tables.sql
+-- Database: PostgreSQL
+-- ============================================
+
+-- Table creation scripts will be added here.
